97c39d648098d78c0b8af60a10742b8ce1d2508a
Parul Pandey <parulss1606@gmail.com>
2023-08-27 14:41:38 +0530

    done with 2nd project

--- Proj2/proj2.js
+++ Proj2/proj2.js
@@ -1,13 +1,29 @@
+//variables
+
+let openBtn = document.getElementById('open-btn');
+let modalContainer = document.getElementById('modal-container');
+let closeBtn = document.getElementById('close-btn');
 
 
+//EventListeners
+
+openBtn.addEventListener('click', function(){
+    modalContainer.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function(){
+    modalContainer.style.display = 'none';
+});
 
 
+//this function will allow the modal to close from anywhere we click on page, callback function is executed.
+window.addEventListener('click', function(e){
+    
+    if(e.target === modalContainer) {
+        modalContainer.style.display = 'none';
+    }
 
-
-
-
-
-
+});
 
 /* Key Concept for this project are: -
 1. document.getElementById()
