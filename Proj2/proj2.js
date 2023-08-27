80f6f6addba9abf296e9be1eb22b43693a9c76f0
Parul Pandey <parulss1606@gmail.com>
2023-08-27 12:23:36 +0530

    new files

---
+++ Proj2/proj2.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+/* Key Concept for this project are: -
+1. document.getElementById()
+2. addEventListener()
+3. e.target
+4. CSS Styling Through JavaScript
+5. CSS Animation through JavaScript. */
